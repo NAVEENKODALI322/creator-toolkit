e24fe82da24a541f6a46eea56ed9544e9654b39b
NAVEENKODALI322 <132426162+NAVEENKODALI322@users.noreply.github.com>
2026-07-05 20:30:46 +0530

    Update thumbnail.js

--- creator-toolkit/api/thumbnail.js
+++ creator-toolkit/api/thumbnail.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  // Allow requests from the Android/iOS app and browser
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   const { videoId } = req.query;
   
   if (!videoId) {
